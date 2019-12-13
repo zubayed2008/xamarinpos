96fbe3687e9b4152b85436e9d54120feda73e532
Md. Zubayed Islam <zubayed2008@yahoo.com>
2019-12-13 08:02:22 +0600

    -removed image

--- PosApp/PosApp/MainPage.xaml.cs
+++ PosApp/PosApp/MainPage.xaml.cs
@@ -19,27 +19,23 @@ namespace PosApp
         public MainPage()
         {
             InitializeComponent();
-            var imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/sports/7/") };
-            imageSource.CachingEnabled = true;
-            imageSource.CacheValidity = TimeSpan.FromHours(1);
-            imagesrc.Source = imageSource;
             userService = new UserService();
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            //var username = Username.Text;
-            //var password = Password.Text;
-            //var role = userService.getUser(username, password);
-            //if (!String.IsNullOrEmpty(role))
-            //{
+            var username = Username.Text;
+            var password = Password.Text;
+            var role = userService.getUser(username, password);
+            if (!String.IsNullOrEmpty(role))
+            {
                 var sales = new Sales();
                 Navigation.PushModalAsync(sales);
-            //}
-            //else
-            //{
-            //    DisplayAlert("Sorry", "User doesn't exist", "OK");
-            //}
+            }
+            else
+            {
+                DisplayAlert("Sorry", "User doesn't exist", "OK");
+            }
         }
     }
 }
